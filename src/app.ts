95815ef888440422f210bcd9bf5559275491166a
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-10-23 16:11:43 +0900

    add index

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static('public'))
+
 app.use(cors())
 
 app.use('/', indexRouter)
