5cd4a38df9fdd3e2a5abc64b63e283e124f7eb05
wonderwall-tim <timothy.chow@wonderwall-g.com>
2024-11-14 17:35:00 +0900

    restruct the dir

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,33 @@
 import dotenv from 'dotenv'
-import app from './app';
+
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import indexRouter from './routes/index';
+import { errorHandler } from './errorHandler';
+import "express-async-errors"
 
 dotenv.config()
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("public"))
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://smart-contract-iota.vercel.app/']
+}))
+
+app.use('/', indexRouter)
+
+app.use(errorHandler)
+
 
 app.listen(3001, () => {
     console.log('app is listening to 3001')
 });
 
 
-export { app }
+module.exports = app;
